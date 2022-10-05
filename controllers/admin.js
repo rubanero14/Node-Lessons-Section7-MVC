13644f4509925040ae735104c9a735397ce98958
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-10-05 17:04:41 +0800

    Pre-Populating the Edit Product Page with Data
    Pre-Populating the Edit Product For with product data when edit button is clicked on products page

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,6 +7,7 @@ exports.getAddProductPage = (req, res, next) => {
     res.render('admin/edit-product', {
         docTitle: 'Add Product',
         path: '/admin/add-product',
+        editing: false,
     })
 }
 
@@ -24,14 +25,21 @@ exports.postAddNewProductPage = (req, res, next) => {
 exports.getEditProductPage = (req, res, next) => {
     // check/verify for query parameters exist using Express's req.query helper here
     const editMode = req.query.edit;
-    
+    console.log(req.query.edit)
     if(!editMode) return res.redirect('/');
 
-    res.render('admin/edit-product', {
-        docTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-    })
+    const prodId = req.params.productId;
+    
+    Product.findById(prodId, product => {
+        if(!product) return res.redirect('/');
+        res.render('admin/edit-product', {
+            docTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product,
+        })
+    });
+
 }
 
 exports.getProducts = (req, res, next) => {
