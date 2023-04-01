ecbb0d9934c7c625252d21554557e4aa383ceded
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-04-01 23:41:23 +0800

    Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,7 +22,7 @@ exports.postAddNewProductPage = async (req, res, next) => {
     price,
     description,
   })
-    .then((result) => console.log(result))
+    .then(() => res.redirect("/admin/products"))
     .catch((err) => console.log(err));
 };
 
@@ -81,6 +81,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then(() => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
