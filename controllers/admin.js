f70f6e2fcc6cd0fc9a3355729a19485524abc1b0
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-04-03 19:28:39 +0800

    Fetching Related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,6 +16,9 @@ exports.postAddNewProductPage = async (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  // the createProduct() method is special method between both User and Product models
+  // The new product creation is wrapped with user profile using this method to associate the product with that particular user id
+  // Refer docs: https://sequelize.org/docs/v6/core-concepts/assocs/#foobelongstobar
   req.user
     .createProduct({
       title,
@@ -35,8 +38,16 @@ exports.getEditProductPage = (req, res, next) => {
 
   const prodId = req.params.productId;
 
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({
+      where: {
+        id: prodId, // This is sequelize conditional fetch similar to 'SELECT * FROM TABLE WHERE ID=prodId'
+      },
+    })
+    // Product.findByPk(prodId)
+    .then((products) => {
+      const product = products[0];
+
       if (!product) return res.redirect("/");
       res.render("admin/edit-product", {
         docTitle: "Edit Product",
@@ -69,7 +80,9 @@ exports.postEditProductPage = async (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+    // Product.findAll()
     .then((products) => {
       res.render("admin/products", {
         products: products,
