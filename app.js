15ae9597fc2c5cec32518ac3ffff78139c0be2f5
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-10-19 01:10:40 +0800

    MongoDB setup into project

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const PORT = 3000;
 const pageNotFoundController = require("./controllers/404");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+
+// // Sequelize DB related codes
+// const sequelize = require("./util/database");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
 
 const app = express();
 // EJS Template Engine Section
@@ -31,12 +33,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Registering generic middleware to enable User data to be used anywhere in the app upon any incoming requests
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user; // storing user data as JSON object inside global Request object under key name 'user', which is created if its not exist
-      next(); // forward to next middleware
-    })
-    .catch((err) => console.log(err));
+  // Sequelize DB related codes
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user; // storing user data as JSON object inside global Request object under key name 'user', which is created if its not exist
+  //     next(); // forward to next middleware
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
 // using outsourced routes from admin.js/shop.js into app.js
@@ -52,54 +55,54 @@ app.use(express.static(path.join(__dirname, "public")));
 // middleware for catching all routes not registered/used and display error 404 message to browser
 app.use(pageNotFoundController.notFoundPage);
 
-//******* DATABASE ASSOCIATIONS *******//
-// Relation setup for Product with User model for User owned product or listed his/her product
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: "CASCADE",
-});
+// //******* DATABASE ASSOCIATIONS *******//
+// // Relation setup for Product with User model for User owned product or listed his/her product for Sequelize
+// Product.belongsTo(User, {
+//   constraints: true,
+//   onDelete: "CASCADE",
+// });
 
-// Relation setup for 1 User has many Products
-User.hasMany(Product);
+// // Relation setup for 1 User has many Products
+// User.hasMany(Product);
 
-// Cart/Order related associations
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-//******** END CODE *********//
+// // Cart/Order related associations
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
+// //******** END CODE *********//
 
-// This code using .sync() translates the model I have defined in database model section in JS object, into SQL table
-// NOTE: Middleware only runs when there is an incoming request, as for the other functions such as sequelize config below will be run only during 'npm start'
-sequelize
-  // .sync({ force: true }) // This will enforce changes of the relational setups into existing tables involved if set to true [Development only, avoid setup force to true in Production]
-  .sync()
-  .then(() => {
-    return User.findByPk(1); // returning here enables forwards to another then block below [Best practice to avoid nested callbacks]
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({
-        name: "Raj",
-        email: "test@email.com",
-      });
-    }
-    return user;
-  })
-  .then((user) => {
-    // Create cart for the particular user globally
-    return user.createCart();
-  })
-  .then((cart) => {
-    // console.log(result);
-    // Listen to server short-hand
-    // console.log(user.toJSON()); // .toJSON() method enables stringified data shown as JSON object without other metadatas from sequelize
-    app.listen(PORT);
-    console.log(`Server online at http://localhost:${PORT}`);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// // This code using .sync() translates the model I have defined in database model section in JS object, into SQL table
+// // NOTE: Middleware only runs when there is an incoming request, as for the other functions such as sequelize config below will be run only during 'npm start'
+// sequelize
+//   // .sync({ force: true }) // This will enforce changes of the relational setups into existing tables involved if set to true [Development only, avoid setup force to true in Production]
+//   .sync()
+//   .then(() => {
+//     return User.findByPk(1); // returning here enables forwards to another then block below [Best practice to avoid nested callbacks]
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       return User.create({
+//         name: "Raj",
+//         email: "test@email.com",
+//       });
+//     }
+//     return user;
+//   })
+//   .then((user) => {
+//     // Create cart for the particular user globally
+//     return user.createCart();
+//   })
+//   .then((cart) => {
+//     // console.log(result);
+//     // Listen to server short-hand
+//     // console.log(user.toJSON()); // .toJSON() method enables stringified data shown as JSON object without other metadatas from sequelize
+//     app.listen(PORT);
+//     console.log(`Server online at http://localhost:${PORT}`);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const { Sequelize } = require("sequelize");
+// // Sequelize DB related codes
+// const { Sequelize } = require("sequelize");
 
-// This area is database connection setup
-// Sequelize constructor takes in 4 args, where it includes database schema name, database id and password and the last args is an object with relevant configs
-const sequelize = new Sequelize("node-lesson", "root", "1234", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// // This area is database connection setup
+// // Sequelize constructor takes in 4 args, where it includes database schema name, database id and password and the last args is an object with relevant configs
+// const sequelize = new Sequelize("node-lesson", "root", "1234", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+// MongoDB related codes
